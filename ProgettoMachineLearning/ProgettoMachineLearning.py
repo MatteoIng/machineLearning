511e188a46a12c3365c3e80021cadbda74c13b9a
MatteoIng <matteofoschi10@gmail.com>
2022-06-22 13:02:27 +0200

    first

--- ProgettoMachineLearning/ProgettoMachineLearning.py
+++ ProgettoMachineLearning/ProgettoMachineLearning.py
@@ -36,11 +36,18 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
-
+valoriLinearTest=[]
+valoriLogisticTest=[]
+valoriRidgeTest=[]
+valoriLassoTest=[]
+valoriSVCTestL1=[]
+valoriSVCTestL2=[]
 
 #0------------------------------------------------------------------------------------------------------------
 #Riduzione dimensionalità
-informazione=1.0
+informazione=0.99
+features=[]
+featuresLinear=[]
 
 while informazione > 0.75:
 
@@ -49,6 +56,7 @@ while informazione > 0.75:
     cumsum = np.cumsum(pca.explained_variance_ratio_)
     d = np.argmax(cumsum >= informazione) + 1
 
+    featuresLinear.append(d)
     print("---------Numero dimensionalita' usate {} con una conservazione del {}% dell'informazione -------------\n".format(d,informazione*100))
     
     pca = PCA(n_components=d)
@@ -58,12 +66,15 @@ while informazione > 0.75:
  
 
     #1--------------------------------------------------------------------------------------------------------
-    #addestro modello lineare 
+    #addestro modello lineare
+    
     print("MODELLO LINEARE")
     modelloLin=linear_model.LinearRegression()
     modelloLin.fit(x_reduced_pca,y_train)
 
-    print("Accuracy score test linear reg:",modelloLin.score(x_reduced_test_pca,y_test))
+    val=modelloLin.score(x_reduced_test_pca,y_test)
+    valoriLinearTest.append(val)
+    print("Accuracy score test linear reg:",val)
     print("Accuracy score training linear reg:",modelloLin.score(x_reduced_pca,y_train))
     print("\n")
 
@@ -71,9 +82,10 @@ while informazione > 0.75:
     #2--------------------------------------------------------------------------------------------------------
     #Addestro modello logistic
     print("LOGISTIC REGRESSION")
-    valoriLogisticTest=[]
+    
     c=0.1
     for i in range(4):
+        features.append(d)
 
         modelloLogistic=linear_model.LogisticRegression(C=c)
         modelloLogistic.fit(x_reduced_pca,y_train)
@@ -93,7 +105,7 @@ while informazione > 0.75:
     #3--------------------------------------------------------------------------------------------------------
     #Addesto linear Reg con costo ridge
     print("LINEAR REGRESSION COSTO RIDGE")
-    valoriRidgeTest=[]
+    
     a=0.1
     for i in range(4):
         
@@ -115,7 +127,7 @@ while informazione > 0.75:
     #4--------------------------------------------------------------------------------------------------------
     #Addesto linear Reg con Lasso
     print("LINEAR REGRESSION LASSO")
-    valoriLassoTest=[]
+    
     a=0.1
     for i in range(4):
 
@@ -137,8 +149,7 @@ while informazione > 0.75:
     #addestro modello SVC
     print("SVC")
     c=0.1
-    valoriSVCTestL1=[]
-    valoriSVCTestL2=[]
+    
     for i in range(4):
 
         modelloSVCl2=LinearSVC(penalty='l2',C=c)
@@ -163,14 +174,48 @@ while informazione > 0.75:
     print("\n")
 
 
-plt.plot([0.1,1,10,100],valoriLogisticTest,color="blue",label="Logistic")
-plt.plot([0.1,1,10,100],valoriRidgeTest,color="black",label="Ridge")
-plt.plot([0.1,1,10,100],valoriLassoTest,color="green",label="Lasso")
+plt.plot(featuresLinear,valoriLinearTest,color="orange",label="Logistic")
+#plt.plot(features,valoriLogisticTest,color="blue",label="Logistic")
+#plt.plot(features,valoriRidgeTest,color="black",label="Ridge")
+#plt.plot(features,valoriLassoTest,color="green",label="Lasso")
 #plt.plot([0.1,1,10,100],valoriSVCTestL1,color="red")
-plt.plot([0.1,1,10,100],valoriSVCTestL2,color="yellow",label="SVCL2")
-plt.xlabel("fitting")
+#plt.plot(features,valoriSVCTestL2,color="yellow",label="SVCL2")
+plt.xlabel("n_features")
 plt.ylabel("Value")
 plt.legend(loc="lower right", title="Legend Title")
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111, projection='3d')
+ax1.plot_trisurf(features, valoriLogisticTest, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], color='white', edgecolors='grey', alpha=0.5)
+ax1.scatter(features, valoriLogisticTest, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], c='red')
+ax1.set_xlabel('n_features')                       
+ax1.set_ylabel('valori_test_accurency')
+ax1.set_zlabel('C_fitting')
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, projection='3d')
+ax2.plot_trisurf(features, valoriRidgeTest, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], color='white', edgecolors='grey', alpha=0.5)
+ax2.scatter(features, valoriRidgeTest, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], c='red')
+ax2.set_xlabel('n_features')                       
+ax2.set_ylabel('valori_test_accurency')
+ax2.set_zlabel('Alpha_fitting')
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111, projection='3d')
+ax3.plot_trisurf(features, valoriLassoTest, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], color='white', edgecolors='grey', alpha=0.5)
+ax3.scatter(features, valoriLassoTest, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], c='red')
+ax3.set_xlabel('n_features')                       
+ax3.set_ylabel('valori_test_accurency')
+ax3.set_zlabel('Alpha_fitting')
+
+fig4 = plt.figure()
+ax4 = fig4.add_subplot(111, projection='3d')
+ax4.plot_trisurf(features, valoriSVCTestL2, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], color='white', edgecolors='grey', alpha=0.5)
+ax4.scatter(features, valoriSVCTestL2, [0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100,0.1,1,10,100], c='red')
+ax4.set_xlabel('n_features')                       
+ax4.set_ylabel('valori_test_accurency')
+ax4.set_zlabel('C_fitting')
+
 plt.show()
 
 
